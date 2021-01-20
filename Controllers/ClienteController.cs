c37fea65c4a5d5faf67829a713fdb4899906a810
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-20 15:13:25 -0300

    ClienteController actions

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 namespace angular_web_api_usage.Controllers
 {
   [Controller]
+  [Route("api/clientes")]
   public class ClienteController
   {
     private readonly AngularWebApiContext _context;
@@ -16,11 +17,69 @@ namespace angular_web_api_usage.Controllers
       _context = context;
     }
 
-    [Route("clientes")]
     [HttpGet]
     public IEnumerable<Cliente> Index()
     {
       return _context.Clientes.AsNoTracking().ToList();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult Show([FromRoute] int id)
+    {
+      var cliente = _context.Clientes
+        .AsNoTracking()
+        .FirstOrDefault(pre => pre.Id == id);
+
+      if (cliente == null)
+      {
+        return new NotFoundResult();
+      }
+
+      return new OkObjectResult(cliente);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] Cliente cliente)
+    {
+      var clienteEncontrado = _context.Clientes.Find(id);
+
+      if (clienteEncontrado == null)
+      {
+        return new BadRequestResult();
+      }
+
+      _context.Entry<Cliente>(cliente).State = EntityState.Modified;
+      _context.SaveChanges();
+
+      return new OkObjectResult(cliente);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult Destroy([FromRoute] int id)
+    {
+      var cliente = _context.Clientes.Find(id);
+
+      if (cliente == null)
+      {
+        return new BadRequestResult();
+      }
+
+      _context.Entry<Cliente>(cliente).State = EntityState.Deleted;
+      _context.SaveChanges();
+
+      return new OkObjectResult(cliente);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Cliente cliente)
+    {
+      _context.Add(cliente);
+      _context.SaveChanges();
+
+      return new OkObjectResult(cliente);
     }
   }
 }
